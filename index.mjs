a1023efb4c363a132052545247b104299ff812c1
farhadJav <114873279+farhadJav@users.noreply.github.com>
2022-11-03 22:41:28 +0500

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,10 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+var name = prompt("What is your name?");
+
+console.log(name);
+
+const num = prompt('Enter a number: ');
+console.log('Your number + 4 =');
+console.log(Number(num) + 4);
