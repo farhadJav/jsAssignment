7a55c09d40e166fb3d69ed6285091e0dff3633aa
farhadJav <114873279+farhadJav@users.noreply.github.com>
2022-11-03 22:43:29 +0500

    Add files via upload

---
+++ prototypes.js
@@ -0,0 +1,13 @@
+
+Array.prototype.print = function(){
+
+    for (let i = 0; i < this.length; i++) {
+        console.log(this[i]);
+      }
+    }
+
+    var check = [0,1,2];
+
+    check.print();
+
+    //another
